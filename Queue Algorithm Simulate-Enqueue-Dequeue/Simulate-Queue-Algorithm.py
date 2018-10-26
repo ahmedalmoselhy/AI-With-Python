548a3cf58c9c98b4f6aa32352716d1e21e9757b2
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2018-10-26 22:55:26 +0200

    Defining a new project

--- Queue Algorithm Simulate-Enqueue-Dequeue/Simulate-Queue-Algorithm.py
+++ Queue Algorithm Simulate-Enqueue-Dequeue/Simulate-Queue-Algorithm.py
@@ -1,18 +1,25 @@
 class queue:
+    # This is a class to simulate Queue Algorithm
     L = []
     def __init__(self):
+        # The Constructor
         print(self.L)
     def enqueue(self,element):
+        # Adding elements to the queue
         self.L.append(element)
     def dequeue(self):
+        # Removing elements from the queue
         if(self.isEmpty() == False):
             return self.L.pop(0)
         else:
             return None
     def count(self):
+        # counting the queue elements
         return len(self.L)
     def isEmpty(self):
+        # check if queue is empty
         return self.count() == 0
+
 
 
 Q1 = queue()

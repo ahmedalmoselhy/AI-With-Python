ba32f3fac6629b43db1ef050217ae869f44b63c7
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2018-10-27 15:33:01 +0200

    finished adding maze

--- Maze-Group-Assignment1/Maze.py
+++ Maze-Group-Assignment1/Maze.py
@@ -152,4 +152,55 @@ game.addMazeSquare(5, 0, False, False, True, False)
 game.addMazeSquare(6, 0, False, True, True, True)
 
 # Second Row
+game.addMazeSquare(0, 1, False, False, False, True)
+game.addMazeSquare(1, 1, False, True, True, True)
+game.addMazeSquare(2, 1, True, True, False, False)
+game.addMazeSquare(3, 1, True, True, False, True)
+game.addMazeSquare(4, 1, True, True, False, False)
+game.addMazeSquare(5, 1, True, False, False, True)
+game.addMazeSquare(6, 1, False, True, True, False)
 
+# Third Row
+game.addMazeSquare(0, 2, True, False, True, False)
+game.addMazeSquare(1, 2, False, False, True, False)
+game.addMazeSquare(2, 2, False, False, False, False)
+game.addMazeSquare(3, 2, False, False, True, True)
+game.addMazeSquare(4, 2, False, True, False, False)
+game.addMazeSquare(5, 2, True, False, True, False)
+game.addMazeSquare(6, 2, False, True, False, True)
+
+# Forth Row
+game.addMazeSquare(0, 3, True, True, False, False)
+game.addMazeSquare(1, 3, True, True, False, False)
+game.addMazeSquare(2, 3, True, False, False, True)
+game.addMazeSquare(3, 3, False, True, True, False)
+game.addMazeSquare(4, 3, True, True, False, False)
+game.addMazeSquare(5, 3, True, False, False, True)
+game.addMazeSquare(6, 3, False, True, True, False)
+
+# Fifth Row
+game.addMazeSquare(0, 4, True, True, False, False)
+game.addMazeSquare(1, 4, True, False, False, True)
+game.addMazeSquare(2, 4, False, True, True, False)
+game.addMazeSquare(3, 4, True, True, False, True)
+game.addMazeSquare(4, 4, True, True, False, False)
+game.addMazeSquare(5, 4, True, True, True, False)
+game.addMazeSquare(6, 4, True, True, False, False)
+
+# Sixth Row
+game.addMazeSquare(0, 5, True, False, False, True)
+game.addMazeSquare(1, 5, False, True, True, True)
+game.addMazeSquare(2, 5, True, False, False, True)
+game.addMazeSquare(3, 5, False, True, True, True)
+game.addMazeSquare(4, 5, True, False, False, True)
+game.addMazeSquare(5, 5, False, True, False, True)
+game.addMazeSquare(6, 5, True, False, False, True)
+
+game.setStartNode(0, 1)
+game.setGoalNode(6, 5)
+
+print("Using Depth First : ")
+print(game.depthFirstTraverse())
+print("\n")
+print("Using Breadth First : ")
+print(game.breadthFirstTraverse())
